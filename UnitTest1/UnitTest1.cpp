11f8f0766ecfbb7b91cd82734eaee1f7adf1a5aa
Карина Печерная <karina.pecherna.03@gmail.com>
2024-10-24 08:50:03 +0300

    s

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "assert.h"
+#include "../lab8.1char/FileName.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+
+			assert(Count("OGOAGA"), 2);  
+			assert(Count("OGOGOGO"), 2);  
+			assert(Count("AGAGAOGO"), 2); 
+			assert(Count("AGAOGOOGO"), 3); 
+		}
+
+
+	};
+}
+	
---
+++ lab8.1char/FileName.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int Count(const string& s)
+{
+    int k = 0;
+    size_t pos = 0;
+    while (pos < s.length()) {
+        if (s.substr(pos, 3) == "OGO" || s.substr(pos, 3) == "AGA") {
+            k++;
+            pos += 3; 
+        }
+        else {
+            pos++; 
+        }
+    }
+    return k;
+}
+
+
+string Change(string& s)
+{
+    size_t pos = 0;
+    while (pos < s.length()) {
+        if (s.substr(pos, 3) == "OGO" || s.substr(pos, 3) == "AGA") {
+            s.replace(pos, 3, "**");
+            pos += 2; 
+        }
+        else {
+            pos++; 
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    cout << "String contains " << Count(str) << " groups of 'OGO' or 'AGA'" << endl;
+
+    string modifiedStr = Change(str);
+
+    cout << "Modified string: " << modifiedStr << endl;
+
+    return 0;
+}
+
